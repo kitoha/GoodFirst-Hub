06a7c2c990e27013b0f95b9e17d486b7507bf2ea
kitoha <kitoha@daum.net>
2025-03-23 22:17:41 +0900

    [master] multi-module로 분리 & db 설정 추가.

--- api/src/main/java/com/goodfirsthub/GoodFirstHubApplication.java
+++ api/src/main/java/com/goodfirsthub/GoodFirstHubApplication.java
@@ -1,13 +1,11 @@
-package com.goodfirsthub.goodfirst_hub;
+package com.goodfirsthub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoodFirstHubApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GoodFirstHubApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GoodFirstHubApplication.class, args);
+  }
 }
