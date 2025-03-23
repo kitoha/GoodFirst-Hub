cc6568f1d2b01fa830cb3f92a5865ddb8aeab008
kitoha <kitoha@daum.net>
2025-03-23 16:55:42 +0900

    first commit

---
+++ src/main/java/com/goodfirsthub/goodfirst_hub/GoodFirstHubApplication.java
@@ -0,0 +1,13 @@
+package com.goodfirsthub.goodfirst_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodFirstHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodFirstHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodfirsthub/goodfirst_hub/GoodFirstHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodfirsthub.goodfirst_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodFirstHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
