e6c211f22c621045faf886b270a91d04781e6318
kitoha <kitoha@daum.net>
2025-04-21 10:48:15 +0900

    [master] PageImpl 직렬화 문제 방지 설정 추가

--- api/src/main/java/com/api/GoodFirstHubApplication.java
+++ api/src/main/java/com/api/GoodFirstHubApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.domain.repository")
 @EntityScan(basePackages = "com.domain.entity")
 @ComponentScan(basePackages = {"com.api", "com.domain"})
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class GoodFirstHubApplication {
   public static void main(String[] args) {
     SpringApplication.run(GoodFirstHubApplication.class, args);
