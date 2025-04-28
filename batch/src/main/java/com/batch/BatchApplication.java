71636a4f8cbfda616a6dc87d9d7f187c0d0bfc96
kitoha <kitoha@daum.net>
2025-04-28 19:06:37 +0900

    [master] Spring Batch 종료 시 exit code 반환 및 웹 서버 비활성화

--- batch/src/main/java/com/batch/BatchApplication.java
+++ batch/src/main/java/com/batch/BatchApplication.java
@@ -1,8 +1,10 @@
 package com.batch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,6 +17,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.batch", "com.domain"})
 public class BatchApplication {
   public static void main(String[] args) {
-    SpringApplication.run(BatchApplication.class, args);
+    int exit = SpringApplication.exit(new SpringApplicationBuilder(BatchApplication.class).web(
+        WebApplicationType.NONE).run(args));
+
+    System.exit(exit);
   }
 }
